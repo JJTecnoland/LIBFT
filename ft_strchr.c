5416eadc9706b561e8cfafa8250029f3790df33e
JJ Tecnoland <jj.tecnoland@gmail.com>
2023-10-25 12:50:45 +0200

    ultimos repasos

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: jlunar-a < jlunar-a@student.42urduliz.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 11:39:38 by jlunar-a          #+#    #+#             */
-/*   Updated: 2023/10/07 14:56:39 by jlunar-a         ###   ########.fr       */
+/*   Updated: 2023/10/25 11:41:36 by jlunar-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,8 @@ contrario devuelve NULL. */
 
 char	*ft_strchr(const char *s, int c)
 {
-	unsigned char	uc;
-	size_t			i ;
-
-	uc = (unsigned char)c;
-	i = 0;
-	while (s[i])
-	{
-		if ((unsigned char)s[i] == uc)
-			return ((char *)s + i);
-		i++;
-	}
-	if (!uc && s[i] == '\0')
-		return ((char *)s + i);
-	return (NULL);
+	while (*s != (unsigned char)c)
+		if (!*s++)
+			return (0);
+	return ((char *)s);
 }
